3f58693216ff8c6e5d05917de5dfc5846976c021
Noahms12 <nm4207@nyu.edu>
2025-10-08 13:35:57 -0400

    Update register_file_tb.v

--- register_file_tb.v
+++ register_file_tb.v
@@ -7,7 +7,8 @@ module register_file_tb;
     reg [1:0] MUX_tgt; // 00:mem_out      01:alu_out      10:pc+1
     reg MUX_rf; //        0:rC(ADD/NAND)  1:rA(SW/BEQ)
     reg WE_rf; //         0:SW/BEQ        1:ADD/ADDI/NAND/LUI/LW/JALR
-    reg [15:0] mem_out, alu_out, pc, instruction;
+    reg [15:0] mem_out, alu_out, pc;
+    reg [2:0] rA, rB, rC;
 
     // Outputs
     wire [15:0] reg_out1, reg_out2;
@@ -21,7 +22,9 @@ module register_file_tb;
         .mem_out(mem_out),
         .alu_out(alu_out),
         .pc(pc),
-        .instruction(instruction),
+        .rA(rA),
+        .rB(rB),
+        .rC(rC),
         .reg_out1(reg_out1),
         .reg_out2(reg_out2)
     );
@@ -67,7 +70,7 @@ module register_file_tb;
         $display("Starting Official RiSC-16 Register File Testbench...");
         
         // 1. Initialize all inputs.
-        WE_rf <= 0; MUX_tgt <= 2'bxx; MUX_rf <= 1'bx; instruction <= 16'h0000;
+        WE_rf <= 0; MUX_tgt <= 2'bxx; MUX_rf <= 1'bx; rA <= 3'b000; rB <= 3'b000; rC <= 3'b000; 
         alu_out <= 16'h0000; mem_out <= 16'h0000; pc <= 16'h0000;
         @(posedge clk); @(posedge clk);
 
@@ -80,7 +83,8 @@ module register_file_tb;
         $display("\n[TEST] LUI writing 0xE380 to R1...");
         WE_rf <= 1; MUX_tgt <= 2'b01; MUX_rf <= 0; alu_out <= 16'hE380; // Assume ALU left shifts imm-10 by 6. MUX_rf val doesn't matter
         // instr: op=011, rA=R1, imm=10b1110001110 = 10'h38E: This value shifts to 1110001110_000000 = 16'hE380 = 58240
-        instruction <= 16'b011_001_1110001110; 
+        rA <= 3'b001;
+        //instruction <= 16'b011_001_1110001110; 
         @(posedge clk); @(posedge clk);
         check_and_print_regs(16'h0, 16'hE380, 16'h0, 16'h0, 16'h0, 16'h0, 16'h0, 16'h0);
         
@@ -90,7 +94,8 @@ module register_file_tb;
         $display("\n[TEST] ADDI adding 7'b1110001 to R1 and store in R3");
         WE_rf <= 1; MUX_tgt <= 2'b01; MUX_rf <= 1; // Assume ALU computes rB(R1) + imm. MUX_rf val doesn't matter
         // instr:     op=001, rA=R3, rB=R1, imm = 7'd113/7'h71
-        instruction <= 16'b001_011_001_1110001;
+        rA <= 3'b011; rB <= 3'b001;
+        //instruction <= 16'b001_011_001_1110001;
         alu_out <= 16'hE3F1; // 16'hE380 + 7'h71 = 58240 + 113 = 58353 = 16'hE3F1
         @(posedge clk); @(posedge clk);
 
@@ -111,7 +116,8 @@ module register_file_tb;
         WE_rf <= 1; MUX_tgt <= 2'b01; alu_out <= 16'hC771;
         MUX_rf <= 0; //MUX_rf chooses the output of rC for the second input
         // instr: op=000, rA=R0, rB=R1, rC=R3
-        instruction <= 16'b000_000_001_0000_011;
+        rA <= 3'b000; rB <= 3'b001; rC <= 3'b011;
+        //instruction <= 16'b000_000_001_0000_011;
         @(posedge clk);
         @(posedge clk);
         // Check SRC1 is correct val
@@ -137,7 +143,8 @@ module register_file_tb;
         WE_rf <= 1; MUX_tgt <= 2'b01; 
         MUX_rf <= 0; //MUX_rf chooses the output of rC for the second input
         // instr: op=010, rA=R5, rB=R1,   rC=R3
-        instruction <= 16'b010_101_001_0000_011;
+        rA <= 3'b101; rB <= 3'b001; rC <= 3'b011;
+        //instruction <= 16'b010_101_001_0000_011;
         alu_out <= ~(16'hE380 & 16'hE3F1); // Result is ~(16'hE380) = 16'h1C7F
         @(posedge clk); @(posedge clk);
         // Check SRC1 is correct val
@@ -162,7 +169,8 @@ module register_file_tb;
         $display("\n[TEST] LW writing 0xBEEF to R6...");
         WE_rf <= 1; MUX_tgt <= 2'b00; mem_out <= 16'hBEEF; //16'hBEEF is hypothetical val at mem loc [rB] + imm
         // instr: op=100, rA=R6, rB=R5
-        instruction <= 16'b100_110_101_0001110; // Loads instruction from memory location [rB] + imm
+        rA <= 3'b110; rB <= 3'b101;
+        //instruction <= 16'b100_110_101_0001110; // Loads instruction from memory location [rB] + imm
         @(posedge clk); @(posedge clk);
         // Check SRC1 is correct val
         $display("\n[TEST OUTPUT 1] reg_out1 should be contents of rB/R5: 16'h1C7F");
@@ -182,7 +190,8 @@ module register_file_tb;
         MUX_rf <= 1; WE_rf <= 0; // For SW
         MUX_tgt <= 2'b00; //doesn't matter
         // instr: op=101, rA=R5 (source), rB=R3, Random imm val
-        instruction <= 16'b101_101_011_0000111; // Storing [R5] in mem loc [R3] + imm
+        rA <= 3'b101; rB <= 3'b011;
+        //instruction <= 16'b101_101_011_0000111; // Storing [R5] in mem loc [R3] + imm
         @(posedge clk); @(posedge clk);
         // Check SRC1 is correct val
         $display("\n[TEST OUTPUT 1] reg_out1 should be contents of rB/R3: 16'hE3F1");
@@ -208,7 +217,8 @@ module register_file_tb;
         $display("\n[TEST] JALR writing PC+1 (0xF00E) to R7...");
         WE_rf <= 1; MUX_tgt <= 2'b10; pc <= 16'hF00D;
         // instr: op=111, rA=R7, rB= R1
-        instruction <= 16'b111_111_001_0000000;
+        rA <= 3'b111; rB <= 3'b001;
+        //instruction <= 16'b111_111_001_0000000;
         @(posedge clk); @(posedge clk);
         // Check SRC1 is correct val
         $display("\n[TEST OUTPUT 1] reg_out1 should be contents of rB/R1: 16'hE380");
@@ -228,7 +238,8 @@ module register_file_tb;
         MUX_rf <= 1; //For BEQ
         WE_rf <= 0; // For BEQ
         // instr: op=110, rA=R1 (source), rB=R3 (source)
-        instruction <= 16'b110_001_011_0000000;
+        rA <= 3'b001; rB <= 3'b011;
+        //instruction <= 16'b110_001_011_0000000;
         @(posedge clk); @(posedge clk);
         // Check SRC1 is correct val
         $display("\n[TEST OUTPUT 1] reg_out1 should be contents of rB/R3: 16'hE3F1");
