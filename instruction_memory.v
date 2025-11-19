e3951387e2f5cfc0260ebf8a63c89050eb115e31
Noahms12 <nm4207@nyu.edu>
2025-11-19 17:19:44 -0500

    Change input from pc to address in instruction_memory

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,6 +1,5 @@
-
 module instruction_memory (
-    input wire [15:0] pc,
+    input wire [15:0] address,
     output wire [15:0] instruction
 );
 
@@ -9,7 +8,7 @@ module instruction_memory (
     // Internal storage for the 65535 memory locations
     reg [15:0] memory [0:65535];
 
-    assign instruction = memory[pc];
+    assign instruction = memory[address];
 
     initial begin
         $readmemh(MEM_FILE, instruction_memory);
