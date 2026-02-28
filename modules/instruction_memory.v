6acf6a5286843862abba6386edd5adf789784184
Noahms12 <nm4207@nyu.edu>
2026-02-28 13:43:06 -0500

    Refactor instruction memory initialization and loading
    Updated memory initialization and loading process.

--- modules/instruction_memory.v
+++ modules/instruction_memory.v
@@ -3,15 +3,23 @@ module instruction_memory (
     output wire [15:0] instruction
 );
 
-    parameter MEM_FILE = "program.mem"; // Defines name of file from which programs will be loaded onto
-
-    // Internal storage for the 65535 memory locations
+    // Internal storage for the 65536 memory locations (0 to 65535)
     reg [15:0] memory [0:65535];
-
+    
     assign instruction = memory[address];
 
+    // integer needed for the looping index
+    integer i;
+
     initial begin
-        $readmemh(MEM_FILE, instruction_memory);
+        //  Initialize all memory locations to 0
+        for (i = 0; i <= 65535; i = i + 1) begin
+            memory[i] = 16'h0000;
+        end
+
+    parameter MEM_FILE = "program.mem"; 
+    //  Load the program data (this will overwrite the 0s at the specific addresses in the file)
+    $readmemh(MEM_FILE, memory);
     end
 
-endmodule 
+endmodule
